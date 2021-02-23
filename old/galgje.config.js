9588e3c351a45cc9510614715bc28b2dbb4b7f5b
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2021-02-23 15:36:44 +0100

    fix: increment game number correctly (#4)
    * chore: remove legacy code
    
    * feat: increment game number properly
    
    * chore: remove debugger call

--- old/galgje.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	apps: [
-    {
-    	name: 'Hangman API',
-    	script: 'hm_server.js',
-			watch: ["hm_server.js", "hm_config.js", "hm_game.js", "hm_wordman.js", "package.json"]
-    }
-  ]
-};
