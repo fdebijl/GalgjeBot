259d48ddfc69ae558b9b86d1edbf85ed0270ff5d
Floris de Bijl <18357629+Fdebijl@users.noreply.github.com>
2021-02-25 16:58:34 +0100

    fix: increase difficulty on win (#12)
    * fix: increase difficulty on win
    
    * test: increase coverage on Game class
    
    * fix: import types in Game test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,9 +2,10 @@
 
 import { games, Game } from '../src/domain/Game';
 import { connect } from '../src/db/connect';
-import { CONFIG } from '../src/config';
+import { CONFIG, isDev } from '../src/config';
 
 (async () => {
-  await connect(CONFIG.MONGO_URL);
+  const mongoUrl = isDev ? CONFIG.MONGO_TEST_URL : CONFIG.MONGO_URL;
+  await connect(mongoUrl);
   games.current = Game.mock();
 })();
