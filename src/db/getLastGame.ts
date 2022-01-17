f8d403ad68a29e201bc801367d26524da8218fed
Floris de Bijl <floris@feedbackfruits.com>
2022-01-17 23:04:36 +0100

    fix: wrong game number being returned

--- src/db/getLastGame.ts
+++ src/db/getLastGame.ts
@@ -4,8 +4,10 @@ import { mog } from './mog';
 export const getLastGame = async (): Promise<Game | void> => {
   const games = await mog.list<Game>({}, {
     collection: 'games',
-    sort: [['gameNumber', -1]]
+    sort: [['gameNumber', 'descending']]
   });
+
+  debugger;
 
   if (games && games.length > 0) {
     return games[0];
